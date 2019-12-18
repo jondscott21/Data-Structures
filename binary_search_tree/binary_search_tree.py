92d8053682aaeeec57a1916fd379c753ada984f2
Jonathan Scott <jonathan.scott@gmail.com>
2019-12-18 00:55:23 -0800

    bst work on for_each

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -14,22 +14,22 @@ class BinarySearchTree:
     def insert(self, value):
         if self.value == None:
             self.value = value
-            return self
+            return self.value
         if self.value < value:
             if self.right is None:
                 self.right = BinarySearchTree(value)
             else:
-                self.right.insert(value)
+                return self.right.insert(value)
         else:
             if self.left is None:
                 self.left = BinarySearchTree(value)
             else:
-                self.left.insert(value)
+                return self.left.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        print(self.value, target)
+        # print(self.value, target)
         if self.value == target:
             return True
         if self.value > target:
@@ -54,7 +54,13 @@ class BinarySearchTree:
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.left is not None:
+            return self.left.for_each(cb)
+        if self.right is not None:
+            return self.right.for_each(cb)
+        else:
+            return
 
     # DAY 2 Project -----------------------
 
@@ -84,15 +90,15 @@ class BinarySearchTree:
     def post_order_dft(self, node):
         pass
 
-bst = BinarySearchTree(5)
+# bst = BinarySearchTree(5)
 
-bst.insert(2)
-bst.insert(3)
-bst.insert(6)
-bst.insert(7)
-bst.insert(10)
-print(bst.contains(10))
-print(bst.contains(8))
+# bst.insert(2)
+# bst.insert(3)
+# bst.insert(6)
+# bst.insert(7)
+# bst.insert(10)
+# print(bst.contains(10))
+# print(bst.contains(8))
 # print(bst.left.right.value)
 # print(bst.right.left.value)
 
